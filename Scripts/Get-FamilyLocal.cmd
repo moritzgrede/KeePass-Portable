babdbf03cf138768753d6b313a4a1a5b799daca4
Moritz Grede <54172651+moritzgrede@users.noreply.github.com>
2021-02-01 00:09:37 +0100

    Initial commit

---
+++ Scripts/Get-FamilyLocal.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO Der lokale Passworttresor ist nicht aufzufinden!
+ECHO Entweder Einrichtung vornehmen oder Fenster über das 'X' schließen
+ECHO(
+PAUSE
+"%~dp0\..\Plugins/IOProtocolExt_WinSCP\WinSCP.exe"
